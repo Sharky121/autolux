28782a3bde24a1b5e60e39d907ec160a4ec2c71e
Sharky <loginov@2cherryline.ru>
2025-01-26 19:30:41 +0300

    driver

--- src/app/driver/page.tsx
+++ src/app/driver/page.tsx
@@ -1,27 +1,27 @@
 import { driversAdvantagesData } from "@/data/drivers-advantages";
 
-// import HeadSection from "@/components/head-section/head-section";
+import HeadSection from "@/components/head-section/head-section";
 import AdvantagesSection from "@/components/advantages-section/advantages-section";
 import DriverFeatures from "@/components/drivers-features/drivers-features";
 
-// async function getData() {
-//   const response = await fetch(`${process.env.HOST}/api/page-data`);
+async function getData() {
+  const response = await fetch(`${process.env.HOST}/api/page-data`);
 
-//   if (!response.ok) {
-//       throw new Error('Failed to fetch data')
-//   }
+  if (!response.ok) {
+      throw new Error('Failed to fetch data')
+  }
 
-//   return response.json();
-// }
+  return response.json();
+}
 
 export default async function Page() {
-  // const pages= await getData();
+  const pages= await getData();
 
-  // const page = pages.find((item: { slug: string; }) => item.slug === 'driver');
+  const page = pages.find((item: { slug: string; }) => item.slug === 'driver');
 
   return (
     <>
-      {/* <HeadSection type="drivers" data={page}/> */}
+      <HeadSection type="drivers" data={page}/>
       <AdvantagesSection title="Преимущества работы в нашей компании" type="drivers" data={driversAdvantagesData}/>
       <DriverFeatures />
     </>
