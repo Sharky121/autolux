15d0b2520313bc2df95a651f29216738e64efe89
Sharky <loginov@2cherryline.ru>
2025-01-26 19:27:44 +0300

    about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,24 @@
-// import HeadSection from "@/components/head-section/head-section";
+import HeadSection from "@/components/head-section/head-section";
 import CallbackSection from "@/components/callback-section/callback-section";
 
-// async function getData() {
-//   const response = await fetch(`${process.env.HOST}/api/page-data`);
+async function getData() {
+  const response = await fetch(`${process.env.HOST}/api/page-data`);
 
-//   if (!response.ok) {
-//       throw new Error('Failed to fetch data')
-//   }
+  if (!response.ok) {
+      throw new Error('Failed to fetch data')
+  }
 
-//   return response.json();
-// }
+  return response.json();
+}
 
 export default async function Page() {
-  // const pages= await getData();
+  const pages= await getData();
 
-  // const page = pages.find((item: { slug: string; }) => item.slug === 'about');
+  const page = pages.find((item: { slug: string; }) => item.slug === 'about');
 
   return (
     <>
-        {/* <HeadSection type="about" data={page}/> */}
+        <HeadSection type="about" data={page}/>
         <CallbackSection />
     </>
   );
